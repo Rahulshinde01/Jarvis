3b90be8d443bc0d21bbec6ffdfc4f9aac9cd5a1d
Rahul Shinde <115041733+Rahulshinde01@users.noreply.github.com>
2025-03-06 14:18:26 +0530

    full project commited

---
+++ musicLibrary.py
@@ -0,0 +1,6 @@
+music = {
+    "goat": "https://www.youtube.com/watch?v=cl0a3i2wFcc",
+    "wavy": "https://www.youtube.com/watch?v=XTp5jaRU3Ws",
+    "softly": "https://www.youtube.com/watch?v=cWMxCE2HTag",
+    "players": "https://www.youtube.com/watch?v=CeFQO9MQNqs"
+}
